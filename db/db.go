67549a3f47c2ac9b2991e1095a2400f260a95df1
squishedfox <christopher.schoeder@protonmail.com>
2024-09-02 21:21:22 -0500

    Fixes import for models in db package

--- db/db.go
+++ db/db.go
@@ -1,6 +1,6 @@
 package db
 
-import "github.com/squishedfox/webservice-prototype/models"
+import "github.com/squishedfox/organization-webservice/models"
 
 type OrganizationQuery struct {
 	// Page to start sorting by. Indexing at 1
